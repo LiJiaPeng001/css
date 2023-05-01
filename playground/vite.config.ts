a21f62a70dcf7a1c27a92089575ea2b4f763c902
lijiapeng001 <1146417576@qq.com>
2023-05-01 15:29:31 +0800

    feat: init

--- playground/vite.config.ts
+++ playground/vite.config.ts
@@ -1,11 +1,4 @@
-import { defineConfig } from 'vite'
-import Vue from '@vitejs/plugin-vue'
-import Unocss from 'unocss/vite'
+import { defineConfig } from "vite";
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [
-    Vue(),
-    Unocss(),
-  ],
-})
+export default defineConfig({});
